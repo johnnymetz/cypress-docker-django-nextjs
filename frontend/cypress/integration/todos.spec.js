86446f070c94d0c3842d0b6a16ae60b4a8e3d5fb
Johnny Metz <jonathan.metz@invitae.com>
2022-01-05 22:29:57 -0800

    Clean up cypress tests

--- frontend/cypress/integration/todos.spec.js
+++ frontend/cypress/integration/todos.spec.js
@@ -1,18 +1,14 @@
-/// <reference types="cypress" />
-
 describe('todo app', () => {
   beforeEach(() => {
     cy.seedDatabase();
     cy.visit('/');
   });
 
-  it('displays todo items', () => {
+  it('displays todos', () => {
     cy.get('.todo-list-item').should('have.length', 2);
-    cy.get('.todo-list-item label').first().should('have.text', 'Walk the dog');
-    cy.get('.todo-list-item label').last().should('have.text', 'Buy groceries');
   });
 
-  it('can add a new todo item', () => {
+  it('can add a new todo', () => {
     const newItem = 'Feed the cat';
     cy.get('#add-todo-input').type(`${newItem}{enter}`);
     cy.get('.todo-list-item label')
@@ -21,22 +17,11 @@ describe('todo app', () => {
       .should('have.text', newItem);
   });
 
-  it('can check an unchecked todo item as done', () => {
+  it('can mark a todo as completed', () => {
     cy.get('input[type=checkbox]:not(:checked)').check().should('be.checked');
   });
 
-  it('can uncheck a checked todo item as not done', () => {
-    cy.get('input[type=checkbox]:checked').uncheck().should('not.be.checked');
-  });
-
-  it('can not delete an uncompleted todo item', () => {
-    cy.get('input[type=checkbox]:not(:checked)')
-      .parentsUntil('.todo-list-item')
-      .contains('Delete')
-      .should('be.disabled');
-  });
-
-  it('can delete a completed todo item', () => {
+  it('can delete a completed todo', () => {
     cy.get('input[type=checkbox]:checked')
       .parentsUntil('.todo-list-item')
       .contains('Delete')
